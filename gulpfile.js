1769ec8cb450de9ea2b56dd4b01c295080c7b014
Sarath <sarath.hacks@gmail.com>
2016-02-06 22:36:37 +0530

    Build script

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var connect = require('gulp-connect');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
+var uglify = require('gulp-uglify');
+
 gulp.task('server', function(){
  connect.server({
      livereload: true  
@@ -37,6 +39,19 @@ gulp.task('script',function(){
    gulp.watch(['./scss/*.scss'],['sass']);
 
  });
+
+ gulp.task('minify',function(){
+
+  return gulp.src('src/*.js')
+    .pipe(uglify({'mangle':true}))
+    .pipe(gulp.dest('dist'));
+
+  });
+
+ /**
+  * Builsd Task
+  */
+ gulp.task('build',['script','minify']);
   
 
   gulp.task('default', ['server','watch']);
